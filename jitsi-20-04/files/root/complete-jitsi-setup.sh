d8ef05f89a8ed1b23e0fae6a94cfd7656ecbdb6a
Gutium Alexei <v-agutium@digitalocean.com>
2022-12-06 16:04:37 +0200

    Fix jitsi app

--- jitsi-20-04/files/root/complete-jitsi-setup.sh
+++ jitsi-20-04/files/root/complete-jitsi-setup.sh
@@ -15,3 +15,16 @@ apt-get -y install jicofo jitsi-meet jitsi-meet-prosody jitsi-meet-turnserver ji
 
 bash /usr/share/jitsi-meet/scripts/install-letsencrypt-cert.sh
 
+# update prosody to use internal user management
+for f in /etc/prosody/conf.avail/*.cfg.lua; do
+    # ignore default prosody configuration files
+	if [[ "$f" == */example.com.cfg.lua ]] || [[ "$f" == */jaas.cfg.lua ]] || [[ "$f" == */localhost.cfg.lua ]]; then
+		continue
+	fi
+	printf "processing prosody conf file: ${f}\n"
+	sed -i "s/authentication = \".*\"/authentication=\"internal_hashed\"/g" $f;
+done
+
+systemctl restart prosody
+systemctl restart jicofo
+systemctl restart jitsi-videobridge2
