bb318d2d1e75e39f25cfae4fa358c7f2fdc8433c
v-aisac <v-aisac@digitalocean.com>
2023-03-03 16:43:18 +0200

    Fixed link on sample page

--- mern-22-04/files/tmp/sample-project/src/App.js
+++ mern-22-04/files/tmp/sample-project/src/App.js
@@ -11,7 +11,7 @@ function App() {
 				</p>
 				<a
 					className="App-link"
-					href="https://marketplace.digitalocean.com/apps/phpmyadmin#getting-started"
+					href="https://marketplace.digitalocean.com/apps/mern#getting-started"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
