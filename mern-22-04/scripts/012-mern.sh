a48298584edee7ebba69c9985557b23d497885e7
v-aisac <v-aisac@digitalocean.com>
2023-03-03 15:46:28 +0200

    Added nodejs user setup and pm2 setup

--- mern-22-04/scripts/012-mern.sh
+++ mern-22-04/scripts/012-mern.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+# Create the nodejs user
+useradd --home-dir /home/nodejs \
+        --shell /bin/bash \
+        --create-home \
+        --system \
+        nodejs
+
+# Setup the home directory
+chown -R nodejs: /home/nodejs
+
+# Setup
+chown -R nodejs: /var/www/html
+
+usermod -aG sudo nodejs
+
 cd /home/
 
 # Create express application
@@ -9,8 +24,15 @@ npx express-generate mern/server
 npx create-react-app mern/client
 
 # Copy sample project
-cd mern/client/src
-mv /sample/src/* .
+cp /tmp/src/* /home/mern/client/src
 
 # Delete sample project
-rm -r /sample
+rm -r /tmp
+
+sudo npm install pm2@latest -g --no-optional
+
+su - nodejs -c "pm2 serve /home/mern/client/src 3000 --name \"sample_mern_app\" --spa"
+sudo env "PATH=$PATH:/usr/bin" /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u nodejs --hp /home/nodejs
+su - nodejs -c "pm2 save"
+
+
