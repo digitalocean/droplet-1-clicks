abeb4a013313553896fc43b7fefb5ca2c488c822
SorokinaVeronika <sorokina.veronika91@gmail.com>
2023-03-24 21:03:16 -0700

    add FARM stack

---
+++ farm-22-04/scripts/011-mongodb.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+distro="$(lsb_release -s -c)"
+
+wget -qO - "https://www.mongodb.org/static/pgp/server-${mongo_repo_version}.asc" | sudo apt-key add -
+
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${distro}/mongodb-org/${mongo_repo_version} multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+
+apt-get -qqy update
+apt-get -qqy install mongodb-org=${mongodb_version} mongodb-org-mongos=${mongodb_version} mongodb-org-server=${mongodb_version} mongodb-org-shell=${mongodb_version} mongodb-org-tools=${mongodb_version} mongodb-mongosh
+
+# Stop MongoDB from updating to an SSPL version until user decides to
+mv -f /etc/apt/sources.list.d/mongodb.list /etc/apt/sources.list.d/mongodb.list.disabled
---
+++ farm-22-04/scripts/012-farm.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Create the farm user
+useradd --home-dir /home/farm \
+        --shell /bin/bash \
+        --create-home \
+        --system \
+        farm
+
+# Setup the home directory
+chown -R farm: /home/farm
+
+# Setup
+chown -R farm: /var/www/html
+
+usermod -aG sudo farm
+
+# Create farm folder
+mkdir /home/farm
+cd /home/farm
+
+# Create react application
+npx create-react-app client
+
+# Copy sample react project
+cp /etc/sample-project/src/* /home/farm/client/src
+
+# Create server
+mkdir server
+cd server
+
+pip install fastapi uvicorn[standard] motor gunicorn pipenv
+
+# Copy sample server
+cp /etc/sample-project/server/* /home/farm/server
+
+pipenv install -r requirements.txt
+ufw allow 8000/tcp
+
+# Delete sample project
+rm -r /etc/sample-project
+rm /home/farm/client/src/logo.svg
+
+cd /home/farm/client/src && npm run build
+
+sudo npm install pm2@latest -g --no-optional
+
+su - farm -c "cd /home/farm/server && pm2 start \"gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app\" --name \"sample_api\""
+su - farm -c "pm2 serve /home/farm/client/build 3000 --name \"sample_farm_app\" --spa"
+sudo env "PATH=$PATH:/usr/bin" /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u farm --hp /home/farm
+su - farm -c "pm2 save"
