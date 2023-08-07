b22bd7eb88c8893c5da794546bc65492e04a6b25
SorokinaVeronika <sorokina.veronika91@gmail.com>
2023-08-07 15:32:50 -0700

    update LinuxGSM

--- linuxgsm-20-04/scripts/011-linuxgsm.sh
+++ linuxgsm-20-04/scripts/011-linuxgsm.sh
@@ -4,24 +4,13 @@ sudo dpkg --add-architecture i386
 sudo apt update
 echo "Y" | sudo apt install lib32gcc1 lib32stdc++6 libsdl2-2.0-0:i386
 
- # allow port for Query and RCON
- ufw allow 27015/tcp
-
- # allow port for Game
- ufw allow 27015/udp
-
- # allow port for SourceTV
- ufw allow 27020/udp
-
- # allow port for Client
- ufw allow 27005/udp
-
 # Create the linuxgsm user
-useradd --home-dir /home/linuxgsm \
-        --shell /bin/bash \
-        --create-home \
-        --system \
-        linuxgsm
+sudo useradd --home-dir /home/linuxgsm \
+            --shell /bin/bash \
+            --create-home \
+            --system \
+            -G sudo \
+            linuxgsm
 
 # Setup the home directory
 chown -R linuxgsm: /home/linuxgsm
