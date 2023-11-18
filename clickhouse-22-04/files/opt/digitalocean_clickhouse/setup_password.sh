9655fb595f3a33008cb4c62569501de5dc896a4c
Gutium Alexei <v-agutium@digitalocean.com>
2023-11-18 23:35:11 +0200

    Fix a little welcome message

--- clickhouse-22-04/files/opt/digitalocean_clickhouse/setup_password.sh
+++ clickhouse-22-04/files/opt/digitalocean_clickhouse/setup_password.sh
@@ -9,8 +9,8 @@ echo ""
 echo "----------------------------------------------------------------------------"
 echo "Your default password is \"$PASSWORD\". To update it you can run:"
 echo "PASSWORD=\$(base64 < /dev/urandom | head -c8); echo \"\$PASSWORD\"; echo -n \"\$PASSWORD\" | sha256sum | tr -d '-'"
-echo "First line is the new password, second is hashed password."
-echo "Update section 'password_sha256_hex' value with generated in the file '/etc/clickhouse-server/users.d/default-password.xml'"
+echo "First line is the new password, second is the hashed password can be used in clickhouse conf file."
+echo "Update section 'password_sha256_hex' value with generated hash in the file '/etc/clickhouse-server/users.d/default-password.xml'"
 echo "----------------------------------------------------------------------------"
 
 cp -f /etc/skel/.bashrc /root/.bashrc
