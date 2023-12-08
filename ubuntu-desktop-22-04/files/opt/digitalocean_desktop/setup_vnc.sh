2ba278bb71facf1924f9ee7b2ab75eb2770b3b87
Gutium Alexei <v-agutium@digitalocean.com>
2023-12-08 08:46:01 +0200

    Add ubuntu desktop 1click

---
+++ ubuntu-desktop-22-04/files/opt/digitalocean_desktop/setup_vnc.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+VNCPASSWORD=$(base64 < /dev/urandom | head -c8)
+USER="user"
+USERPASSWORD=$(base64 < /dev/urandom | head -c8)
+
+echo "Ubuntu-desktop is now installed and configured. Enjoy it!"
+echo ""
+echo "----------------------------------------------------------------------------"
+echo "Your VNC password is \"$VNCPASSWORD\". To update it you can run:"
+echo "x11vnc -storepasswd %NEW_PASSWORD% /home/$USER/.vnc/passwd"
+echo "Using XServer from root account is not recommended, so user \"$USER\" is created with password \"$USERPASSWORD\""
+echo "To change user's password execute 'passwd ${USER}'"
+echo "Raw generated authentication data is stored in '/root/.vnc_password'"
+echo "----------------------------------------------------------------------------"
+echo "Ubuntu-Desktop is starting, please wait about 2-3 minutes until console is unblocked"
+
+cat >> /root/.vnc_password <<EOM
+VNC_PASSWORD=$VNCPASSWORD
+USER=$USER
+USER_PASSWORD=$USERPASSWORD
+EOM
+
+adduser --gecos "" --disabled-password $USER
+chpasswd <<<"$USER:$USERPASSWORD"
+
+sudo -u $USER mkdir /home/user/.vnc
+x11vnc -storepasswd $VNCPASSWORD /home/$USER/.vnc/passwd
+
+systemctl stop x11vnc
+systemctl stop sddm
+systemctl enable x11vnc
+systemctl enable sddm
+systemctl start sddm
+systemctl start x11vnc
+
+cp -f /etc/skel/.bashrc /root/.bashrc
---
+++ ubuntu-desktop-22-04/scripts/010-ubuntu-desktop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Scripts in this directory are run during the build process.
+# each script will be uploaded to /tmp on your build droplet, 
+# given execute permissions and run.  The cleanup process will
+# remove the scripts from your build system after they have run
+# if you use the build_image task.
+#
+
+# open default VNC port
+ufw allow 5900
+# Add first-login task
+cat >> /root/.bashrc <<EOM
+/opt/digitalocean_desktop/setup_vnc.sh
+EOM
