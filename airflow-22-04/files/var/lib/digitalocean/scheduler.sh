445939aab79a4c359727e9350aad044ef247eef7
Gutium Alexei <alexey.lakris@gmail.com>
2024-03-22 16:37:55 +0200

    airflow 1click (#163)
    * airflow droplet
    
    
    ---------
    
    Co-authored-by: Gutium Alexei <v-agutium@digitalocean.com>

---
+++ airflow-22-04/files/var/lib/digitalocean/scheduler.sh
@@ -0,0 +1,2 @@
+source /home/airflow/airflow-project/airflow-env/bin/activate
+airflow scheduler
---
+++ airflow-22-04/files/var/lib/digitalocean/webserver.sh
@@ -0,0 +1,2 @@
+source /home/airflow/airflow-project/airflow-env/bin/activate
+airflow webserver -p 8080
