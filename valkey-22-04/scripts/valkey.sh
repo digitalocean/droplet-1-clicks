a8ff90bea96d1924a0c3684da14f656ab35251c0
Scott Miller <scottmiller@digitalocean.com>
2024-05-17 15:03:38 -0600

    Security changes, other optimizations

--- valkey-22-04/scripts/valkey.sh
+++ valkey-22-04/scripts/valkey.sh
@@ -2,19 +2,23 @@
 
 # open port for clients
 ufw allow 6379
+ufw limit ssh/tcp
 ufw allow ssh
 ufw --force enable
 
-# make directory
-mkdir /var/lib/digitalocean
+# Get the latest version of valkey
+tag_version=$(curl -s "https://api.github.com/repos/valkey-io/valkey/releases/latest" | jq -r '.tag_name')
+echo "Latest version of valkey is $tag_version"
 
 #clone valkey project
 mkdir /srv
 cd /srv
-git clone --depth 1 https://github.com/valkey-io/valkey.git
+git clone https://github.com/valkey-io/valkey.git
+cd valkey
+git fetch --tags
+git checkout tags/$tag_version
 
 # build valkey
-cd /srv/valkey
 make distclean
 
 # install valkey
