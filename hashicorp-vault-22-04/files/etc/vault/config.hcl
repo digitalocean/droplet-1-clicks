31e2cf5ef17b992e53ef32ad9e684e6408b1bbbd
Gutium Alexei <v-agutium@digitalocean.com>
2024-05-22 13:11:32 +0300

    Address security review comments

---
+++ hashicorp-vault-22-04/files/etc/vault/config.hcl
@@ -0,0 +1,11 @@
+storage "consul" {
+  address = "127.0.0.1:8500"
+  path    = "vault/"
+}
+
+listener "tcp" {
+  address     = "0.0.0.0:8200"
+  tls_disable = 1
+}
+
+ui = true
--- hashicorp-vault-22-04/scripts/012-vault.sh
+++ hashicorp-vault-22-04/scripts/012-vault.sh
@@ -3,10 +3,22 @@
 # non-interactive install
 export DEBIAN_FRONTEND=noninteractive
 
-# Install hashicorp vault
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+wget https://releases.hashicorp.com/consul/1.17.3/consul_1.17.3_linux_amd64.zip
+unzip consul_1.17.3_linux_amd64.zip
+sudo mv consul /usr/bin
 
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+wget https://releases.hashicorp.com/vault/1.14.8/vault_1.14.8_linux_amd64.zip
+unzip vault_1.14.8_linux_amd64.zip
+sudo mv vault /usr/bin
+
+cat >> /root/.bashrc <<EOM
+# generate token and keys
+echo "HashiCorp Vault is being initialized"
+sleep 5
+vault operator init -address=http://127.0.0.1:8200 > /.digitalocean_vault_tokens.txt
+cp -f /etc/skel/.bashrc /root/.bashrc
+echo "HashiCorp Vault is successfully initialized"
+EOM
 
 # Allow elasticsearch port
 ufw limit ssh
