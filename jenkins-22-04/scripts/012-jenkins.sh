8cf0c19275c8bbc6a1a71198c120139f2fe15c70
Gutium Alexei <v-agutium@digitalocean.com>
2024-06-06 17:44:28 +0300

    Use nginx as reverse proxy for jenkins

---
+++ jenkins-22-04/scripts/012-jenkins.sh
@@ -0,0 +1,12 @@
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+apt -qqy update
+
+ufw allow http
+ufw allow https
+ufw limit ssh
+ufw --force enable
