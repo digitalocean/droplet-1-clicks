2fcff62cbaf09de60ee69665f93753b881bff67e
mbeyt <mbeyt@digitalocean.com>
2024-06-07 19:43:50 +0000

    fetch latest version for docker-compose

--- common/scripts/011-docker-compose.sh
+++ common/scripts/011-docker-compose.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 mkdir -p ~/.docker/cli-plugins/;
+if [ -z "${docker_compose_version}" ]; then
+  docker_compose_version=$(curl -sSL "https://api.github.com/repos/docker/compose/releases/latest" | jq -r '.tag_name');
+fi
 curl -SL https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose;
 chmod +x ~/.docker/cli-plugins/docker-compose;
