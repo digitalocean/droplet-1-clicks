2cac5fc8a5e18da024cb9283611150929aaf1c10
Scott Miller <scottmiller@digitalocean.com>
2024-12-02 14:20:21 -0700

    add-open-webui

---
+++ open-webui-22-04/scripts/open-webui.sh
@@ -0,0 +1,11 @@
+
+#!/bin/sh
+
+# open port for clients
+ufw allow 3000
+ufw limit ssh/tcp
+ufw allow ssh
+ufw --force enable
+
+# Get the latest version of open-webui
+docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
