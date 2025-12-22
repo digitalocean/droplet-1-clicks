8ac7107939bbe1b05c386437ea11b4764f5d24df
Gutium Alexei <v-agutium@digitalocean.com>
2025-12-22 20:19:09 +0200

    Update mongodb to 8.0.17

--- mongodb-24-04/scripts/010-mongodb.sh
+++ mongodb-24-04/scripts/010-mongodb.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 #MongoDB doesn’t yet publish a “noble” repo, but the jammy (22.04) packages work fine on 24.04.
-#distro="$(lsb_release -s -c)"
-distro="jammy"
+distro="$(lsb_release -s -c)"
 
-wget -qO - "https://www.mongodb.org/static/pgp/server-${repo_version}.asc" | sudo apt-key add -
+curl -fsSL https://www.mongodb.org/static/pgp/server-${repo_version}.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-${repo_version}.gpg \
+   --dearmor --yes
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu ${distro}/mongodb-org/${repo_version} multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
-
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-${repo_version}.gpg ] https://repo.mongodb.org/apt/ubuntu ${distro}/mongodb-org/${repo_version} multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-${repo_version}.list
 ufw allow 27017/tcp
 
 apt-get -qqy update
-apt-get -qqy install mongodb-org=${mongodb_version} mongodb-org-mongos=${mongodb_version} mongodb-org-server=${mongodb_version} mongodb-org-shell=${mongodb_version} mongodb-org-tools=${mongodb_version}
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" mongodb-org=${mongodb_version} mongodb-org-mongos=${mongodb_version} mongodb-org-server=${mongodb_version} mongodb-org-shell=${mongodb_version} mongodb-org-tools=${mongodb_version}
 
 # Stop MongoDB from updating to an SSPL version until user decides to
-mv -f /etc/apt/sources.list.d/mongodb.list /etc/apt/sources.list.d/mongodb.list.disabled
+sudo mv /etc/apt/sources.list.d/mongodb-org-${repo_version}.list /etc/apt/sources.list.d/mongodb-org-${repo_version}.list.disabled
