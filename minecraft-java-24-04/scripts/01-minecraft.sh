ed9bb177106f94c8308bbba4b2c811853b6d1c6a
Mahesh Nerkar <v-mnerkar@digitalocean.com>
2026-07-08 21:58:40 +0530

    upgrade to minecraft java and ubuntu 24.04

--- minecraft-java-24-04/scripts/01-minecraft.sh
+++ minecraft-java-24-04/scripts/01-minecraft.sh
@@ -3,14 +3,10 @@
 # non-interactive install
 export DEBIAN_FRONTEND=noninteractive
 
-sudo add-apt-repository -y ppa:openjdk-r/ppa
-sudo apt update
-sudo apt install -y openjdk-17-jre-headless
-
 groupadd minecraft
 useradd --system --shell /usr/sbin/nologin --home /opt/minecraft -g minecraft minecraft
 
-wget https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar -P /opt/minecraft
+wget https://piston-data.mojang.com/v1/objects/823e2250d24b3ddac457a60c92a6a941943fcd6a/server.jar -P /opt/minecraft
 
 echo "eula=true" > /opt/minecraft/eula.txt
 
