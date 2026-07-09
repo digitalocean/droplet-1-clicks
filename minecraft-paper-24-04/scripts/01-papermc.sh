cd0cfb6b543b7129f8fb927234ab38137a5dfebd
v-rvaigankar <v-rvaigankar@digitalocean.com>
2026-07-09 12:19:38 +0530

    upd: update the package version to latest

--- minecraft-paper-24-04/scripts/01-papermc.sh
+++ minecraft-paper-24-04/scripts/01-papermc.sh
@@ -3,9 +3,11 @@
 # non-interactive install
 export DEBIAN_FRONTEND=noninteractive
 
-sudo add-apt-repository -y ppa:openjdk-r/ppa
-sudo apt update
-sudo apt install -y openjdk-21-jre-headless curl
+apt -qqy install wget apt-transport-https gpg curl
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
+echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+apt -qqy update
+apt -qqy -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' install temurin-25-jre
 
 groupadd minecraft
 useradd --system --shell /usr/sbin/nologin --home /opt/minecraft -g minecraft minecraft
@@ -13,13 +15,13 @@ useradd --system --shell /usr/sbin/nologin --home /opt/minecraft -g minecraft mi
 mkdir -p /opt/minecraft
 
 PAPER_URL=$(curl -fsSL -H "User-Agent: digitalocean-droplet-1-clicks/1.0 (marketplace@digitalocean.com)" \
-  "https://fill.papermc.io/v3/projects/paper/versions/1.21.4/builds/224" \
+  "https://fill.papermc.io/v3/projects/paper/versions/26.1.2/builds/74" \
   | python3 -c "import sys, json; print(json.load(sys.stdin)['downloads']['server:default']['url'])")
 
-wget -q "${PAPER_URL}" -O /opt/minecraft/paper-1.21.4-224.jar
+wget -q "${PAPER_URL}" -O /opt/minecraft/paper-26.1.2-74.jar
 
 echo "eula=true" > /opt/minecraft/eula.txt
 
 chown -R minecraft:minecraft /opt/minecraft
 chown -R minecraft:minecraft /opt/minecraft/eula.txt
-chown -R minecraft:minecraft /opt/minecraft/paper-1.21.4-224.jar
+chown -R minecraft:minecraft /opt/minecraft/paper-26.1.2-74.jar
