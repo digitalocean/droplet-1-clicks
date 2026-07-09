681e922dc17a145dc8ecd8621cc49986b32096df
v-agaonkar <v-agaonkar@digitalocean.com>
2026-07-09 10:50:18 +0530

    Upgraded Minecraft Forge version to 26.1.2-64.0.11.

--- minecraft-forge-24-04/scripts/01-minecraft.sh
+++ minecraft-forge-24-04/scripts/01-minecraft.sh
@@ -10,13 +10,13 @@ wget https://piston-data.mojang.com/v1/objects/823e2250d24b3ddac457a60c92a6a9419
 
 echo "eula=true" > /opt/minecraft/eula.txt
 
-wget https://maven.minecraftforge.net/net/minecraftforge/forge/26.2-65.0.3/forge-26.2-65.0.3-installer.jar -P /opt/minecraft
+wget https://maven.minecraftforge.net/net/minecraftforge/forge/26.1.2-64.0.11/forge-26.1.2-64.0.11-installer.jar -P /opt/minecraft
 
 chown -R minecraft:minecraft /opt/minecraft
 chown -R minecraft:minecraft /opt/minecraft/eula.txt
 chown -R minecraft:minecraft /opt/minecraft/server.jar
 
-sudo -u minecraft java -jar /opt/minecraft/forge-26.2-65.0.3-installer.jar --installServer
+sudo -u minecraft java -jar /opt/minecraft/forge-26.1.2-64.0.11-installer.jar --installServer
 
 ufw limit ssh
 ufw allow 25565/tcp
