ab7a3d95a6d405fcfd05124663772bc5ca3d84d8
v-mnerkar-do <v-mnerkar@digitalocean.com>
2026-09-18 09:27:19 +0530

    MP-8841 - Keycloak: Fix version sync, pin to 26.7.4, and bump build size (#403)

--- keycloak-24-04/scripts/01-keycloak.sh
+++ keycloak-24-04/scripts/01-keycloak.sh
@@ -2,6 +2,11 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+: "${application_version:?application_version must be set}"
+
+# Pin both Keycloak base image tags from Packer application_version (full semver)
+sed -i "s|KEYCLOAK_VERSION_PLACEHOLDER|${application_version}|g" /var/digitalocean/Dockerfile
+
 systemctl start docker
 systemctl enable docker
 
