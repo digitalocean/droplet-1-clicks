bd2972fc196ecb031891067fd925cb65dad1757d
Mahesh Nerkar <v-mnerkar@digitalocean.com>
2026-09-23 11:49:37 +0530

    Autoupdate fix

--- discourse-24-04/scripts/010-discourse.sh
+++ discourse-24-04/scripts/010-discourse.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 #
 # Scripts in this directory are run during the build process.
 # each script will be uploaded to /tmp on your build droplet, 
@@ -6,8 +7,8 @@
 # remove the scripts from your build system after they have run
 # if you use the build_image task.
 #
-mkdir /var/discourse
-git clone https://github.com/discourse/discourse_docker.git /var/discourse
+mkdir -p /var/discourse
+git clone --depth 1 https://github.com/discourse/discourse_docker.git /var/discourse
 
 # Add first-login task
 cat >> /root/.bashrc <<EOM
