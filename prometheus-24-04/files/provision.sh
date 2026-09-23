704771d4dda00bdcb3f138ada1baa32cb38ce39c
v-agaonkar <v-agaonkar@digitalocean.com>
2026-09-23 17:07:41 +0530

    MP-8861 Prometheus version upgrade

--- prometheus-24-04/files/provision.sh
+++ prometheus-24-04/files/provision.sh
@@ -13,7 +13,7 @@ sudo useradd -s /sbin/nologin --system -g prometheus prometheus
 sudo mkdir -p /etc/prometheus /var/lib/prometheus
 
 # Download and extract Prometheus
-curl -sSL https://github.com/prometheus/prometheus/releases/download/v2.54.1/prometheus-2.54.1.linux-amd64.tar.gz | tar -xz
+curl -sSL "https://github.com/prometheus/prometheus/releases/download/v${application_version}/prometheus-${application_version}.linux-amd64.tar.gz" | tar -xz
 
 # Move Prometheus binaries to /usr/local/bin
 sudo mv /root/prometheus*/{prometheus,promtool} /usr/local/bin
@@ -23,7 +23,8 @@ sudo chown prometheus:prometheus /usr/local/bin/prometheus
 sudo chown prometheus:prometheus /usr/local/bin/promtool
 
 # Move configuration files and change ownership
-sudo mv /root/prometheus*/{consoles,console_libraries,prometheus.yml} /etc/prometheus
+# Note: Prometheus 3.x no longer ships example consoles/console_libraries
+sudo mv /root/prometheus*/prometheus.yml /etc/prometheus
 sudo chown -R prometheus:prometheus /etc/prometheus /var/lib/prometheus
 
 # Reload systemd to recognize the new service
